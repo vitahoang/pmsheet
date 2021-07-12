e2df9fabc1ae50dc9b327b6cfd20a43f249d45e2
vitahoang <hunghoang.vita@gmail.com>
2021-07-12 11:42:53 +0700

    feat(decorator.js): change the border style of the timeline & rename f@decorAday -> decorDay

--- src/decorator.js
+++ src/decorator.js
@@ -6,7 +6,7 @@
 */
 function decorLastWeek(maxCol, maxRow, sheet) {
   sheet.getRange(1, maxCol - 9, maxRow, 10)
-    .setBorder(false, true, false, true, null, null, "gray", SpreadsheetApp.BorderStyle.SOLID_MEDIUM)
+    .setBorder(false, true, false, true, null, null, "#cccccc", SpreadsheetApp.BorderStyle.SOLID_MEDIUM)
     .setHorizontalAlignment("center");
 }
 
@@ -19,7 +19,7 @@ function decorLastDay(sheet) {
   var maxCol = sheet.getMaxColumns();
   var maxRow = sheet.getMaxRows();
   sheet.getRange(1, maxCol - 1, maxRow - 1, 2)
-    .setBorder(false, true, false, true, false, false, "gray", SpreadsheetApp.BorderStyle.DOTTED);
+    .setBorder(true, true, true, true, true, true, "#cccccc", SpreadsheetApp.BorderStyle.DOTTED);
 }
 
 
@@ -27,12 +27,12 @@ function decorLastDay(sheet) {
 * Decorate a daily column of the Gantt timeline.
 * @param   {Object} dailyRange A range object which represents for a daily column.
 */
-function decorAday(dailyRange) {
+function decorDay(dailyRange) {
   var maxRow = dailyRange.getValues().length;
   var rangeHeader = dailyRange.offset(0, 0, 1, 2);
   var rangeBody = dailyRange.offset(1, 0, maxRow - 1, 2);
   rangeBody.setBorder(false, false, false, false, false, false);
-  dailyRange.setBorder(false, true, false, true, false, false, "gray", SpreadsheetApp.BorderStyle.DOTTED);
+  dailyRange.setBorder(true, true, true, true, true, true, "#cccccc", SpreadsheetApp.BorderStyle.DOTTED);
   rangeHeader.setTextStyle(dailyHeaderStyle);
 }
 
